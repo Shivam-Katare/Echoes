6a4aca2c0ade2d8ba960b9e589cd9e2bf728a0a5
Shivam-Katare <katare27451@gmail.com>
2024-11-17 22:58:01 +0530

    create word generator api

--- echo-api/assembly/index.ts
+++ echo-api/assembly/index.ts
@@ -1,5 +1,6 @@
-// export * from "./qoutes";
+export * from "./qoutes";
+export * from "./words";
 
-// export function sayHello(name: string | null = null): string {
-//   return `Hello, ${name || "World"}!`;
-// }
+export function sayHello(name: string | null = null): string {
+  return `Hello, ${name || "World"}!`;
+}
